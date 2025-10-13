4459bca2e0e3c6de101a8d1663384ed35f15e0e5
Cory Madden <maddencs@gmail.com>
2025-10-12 20:03:48 -0700

    linting

--- src/services/order.py
+++ src/services/order.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.models import Order
-from src.services.order_scheduling import OrderScheduleRunner, OrderScheduler
+from src.services.order_scheduling import OrderScheduler, OrderScheduleRunner
 
 
 async def submit_order(

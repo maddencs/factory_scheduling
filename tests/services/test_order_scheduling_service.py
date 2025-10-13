bcd57dc5d99c603f73d79e53c9293d7fc92735f3
Cory Madden <maddencs@gmail.com>
2025-10-12 17:28:59 -0700

    add test for multi workcenter orders

--- tests/services/test_order_scheduling_service.py
+++ tests/services/test_order_scheduling_service.py
@@ -97,3 +97,62 @@ async def test_order_service_schedule_parts__workcenter_with_one_scheduled_part(
     assert new_scheduled_part.order_id == new_order.id
     assert new_scheduled_part.scheduled_start is not None
     assert new_scheduled_part.scheduled_start == prior_scheduled_part.scheduled_start + part.lead_time
+
+
+@pytest.mark.asyncio
+async def test_order_service_schedule_parts__multi_workcenter(db_session):
+    """
+    Schedule parts of different lead times for two workcenters that don't have any active scheduled parts
+    """
+    cranium_workcenter = await workcenter_factory(
+        session=db_session,
+        name="Cranium Workcenter",
+    )
+    torso_workcenter = await workcenter_factory(
+        session=db_session,
+        name="Torso Workcenter",
+    )
+    bom = await bom_factory(
+        session=db_session,
+        name="Robot",
+    )
+    cranium_lead_time = timedelta(days=1)
+    cranium_part = await part_factory(
+        name="Cranium",
+        lead_time=cranium_lead_time,
+        workcenter_id=cranium_workcenter.id,
+        session=db_session,
+    )
+    await bom_part_factory(
+        part_id=cranium_part.id,
+        bill_of_materials_id=bom.id,
+        quantity=1,
+        session=db_session,
+    )
+
+    torso_lead_time = timedelta(days=2)
+    torso_part = await part_factory(
+        name="Cranium",
+        lead_time=torso_lead_time,
+        workcenter_id=torso_workcenter.id,
+        session=db_session,
+    )
+    await bom_part_factory(
+        part_id=torso_part.id,
+        bill_of_materials_id=bom.id,
+        quantity=1,
+        session=db_session,
+    )
+
+    order = await order_factory(bill_of_materials_id=bom.id, session=db_session)
+
+    await OrderScheduler().schedule(order, db_session)
+
+    result = await db_session.execute(select(ScheduledPart))
+    scheduled_parts = result.scalars().all()
+    torso_scheduled_part = [sp for sp in scheduled_parts if sp.part_id == torso_part.id][0]
+    cranium_scheduled_part = [sp for sp in scheduled_parts if sp.part_id == cranium_part.id][0]
+
+    assert cranium_scheduled_part.scheduled_start == (
+        torso_scheduled_part.scheduled_start + torso_lead_time - cranium_lead_time
+    )
